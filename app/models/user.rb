edd3796c41ad0bf886b765c4f4682c6798847656
mokxter <ianbuena081191@gmail.com>
2015-01-06 00:02:47 +0800

    Add method to User model that interacts with Omniauth's object

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.first_name = auth.info.first_name
+      user.email = auth.info.email
+      user.oauth_token = auth.creadentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save
+    end
+  end
+
+  def create
+  end
 end
