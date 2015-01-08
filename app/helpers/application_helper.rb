da73b3d154e75d6b4722928768a953d18836309f
mokxter <ianbuena081191@gmail.com>
2015-01-08 16:24:14 +0800

    Add facebook meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  # Returns a full title for the application
+  def full_title(page_title = '')
+    base_title = "To-do App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  # Returns an og description
+  def og_description(page_description = '')
+    base_description = "A simple to-do list app made from rails"
+    if page_description.empty?
+      base_description
+    else
+      page_description
+    end
+  end
+
+  private
 end
